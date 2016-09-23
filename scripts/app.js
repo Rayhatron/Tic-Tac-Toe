68633ef911c1d1df06ca6ac4ae50493329dd3425
rayhatron <rayhatron@gmail.com>
2016-09-23 10:19:31 +0200

    "Added choose symbol function & board entry logic"

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,29 @@ angular.module("ngTicTacToe", []);
   angular
     .module("ngTicTacToe")
     .controller("ctrTicTacToe", function($scope) {
+      $scope.userSymbol = "";
+      $scope.aiSymbol = "";
+      $scope.hideWelcomeScreen = false;
+      $scope.hideBoard = true;
+      
+      $scope.playerSymbol = function(symbol){
+        switch (symbol) {
+          case 'X':
+            $scope.userSymbol = "X";
+            $scope.aiSymbol = "O";
+            break;
+          case 'O':
+            $scope.userSymbol = "O";
+            $scope.aiSymbol = "X";
+          default:
 
+        }
+
+        $scope.hideWelcomeScreen = true;
+        $scope.hideBoard = false;
+
+        console.log($scope.userSymbol + " and " + $scope.aiSymbol);
+      }
 
 
 
